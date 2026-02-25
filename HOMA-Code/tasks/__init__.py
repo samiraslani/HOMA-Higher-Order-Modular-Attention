17a759cca5526ec99350931b56c3c1fc71c64dd3
samiraslani <shirin01@my.yorku.ca>
2026-02-25 16:23:58 -0500

    Create __init__.py

---
+++ HOMA-Code/tasks/__init__.py
@@ -0,0 +1,5 @@
+from .secondary_structure import SecondaryStructureTask
+from .fluorescence import FluorescenceTask
+from .stability import StabilityTask
+
+__all__ = ["SecondaryStructureTask", "FluorescenceTask", "StabilityTask"]
